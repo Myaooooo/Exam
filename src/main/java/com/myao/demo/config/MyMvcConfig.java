b80432e86a00e9504f238d2806a883fb1372dfdf
Myao <464203784@qq.com>
2020-11-30 10:23:24 +0800

    修改Layui版本为2.5.7

--- src/main/java/com/myao/demo/config/MyMvcConfig.java
+++ src/main/java/com/myao/demo/config/MyMvcConfig.java
@@ -1,6 +1,7 @@
 package com.myao.demo.config;
 
 import org.springframework.context.annotation.Configuration;
+import org.springframework.web.servlet.config.annotation.ResourceHandlerRegistry;
 import org.springframework.web.servlet.config.annotation.ViewControllerRegistry;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 @Configuration
@@ -16,5 +17,19 @@ public class MyMvcConfig implements WebMvcConfigurer {
             registry.addViewController("/").setViewName("index");
             registry.addViewController("/index.html").setViewName("index");
         }
+    @Override
+    public void addResourceHandlers(ResourceHandlerRegistry registry) {
+        if (!registry.hasMappingForPattern("/webjars/**")) {
+            registry.addResourceHandler("/webjars/**").addResourceLocations(
+                    "classpath:/META-INF/resources/webjars/");
+        }
+        if (!registry.hasMappingForPattern("/**")) {
+            registry.addResourceHandler("/**").addResourceLocations(
+                    "classpath:/META-INF/resources/", "classpath:/resources/",
+                    "classpath:/static/", "classpath:/public/");
+        }
 
     }
+
+
+}
