c32f74112eef38e13bb5528518ce04376189d99b
Myao <464203784@qq.com>
2020-12-04 17:36:47 +0800

    添加跳转到注册页面

--- src/main/java/com/myao/demo/controller/LoginController.java
+++ src/main/java/com/myao/demo/controller/LoginController.java
@@ -8,6 +8,12 @@ import org.springframework.web.bind.annotation.ResponseBody;
 @Controller
 public class LoginController {
 
+    @RequestMapping("/register")
+    public String register() {
+        System.out.println("跳转到注册页面");
+        return "register";
+    }
+
     @RequestMapping("/login")
     @ResponseBody
     public String  login(User user){
